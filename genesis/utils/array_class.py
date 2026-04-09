79978126b5d2592826c833ac06f45fc18e233370
Mingrui Zhang <33411325+erizmr@users.noreply.github.com>
2026-04-09 06:08:19 +0100

    [MISC] Speed up rigid constraint solver init. (#2521)

--- genesis/utils/array_class.py
+++ genesis/utils/array_class.py
@@ -2022,6 +2022,7 @@ class StructRigidSimStaticConfig(metaclass=AutoInitMeta):
     integrator: int
     solver_type: int
     requires_grad: bool
+    parallel_init: bool = False  # parallelize init over (constraints, envs) when GPU is not saturated by envs alone
     enable_tiled_cholesky_mass_matrix: bool = False
     enable_tiled_cholesky_hessian: bool = False
     tiled_n_dofs_per_entity: int = -1
