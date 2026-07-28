7af02819da4c6820b291c41f32d72429d1e77e4a
Zhihui Du <zhdu_amdeng@amd.com>
2026-07-27 23:03:08 -0400

    perf: opt-maxpairs-4 on PR85-17c base

--- genesis/options/solvers.py
+++ genesis/options/solvers.py
@@ -489,7 +489,7 @@ class RigidOptions(Options):
     enable_neutral_collision: StrictBool = False
     enable_adjacent_collision: StrictBool = False
     disable_constraint: StrictBool = False
-    max_collision_pairs: NonNegativeInt = 32  # OPT: G1 has only 4 contact pairs, 150 is excessive (+0.81%)
+    max_collision_pairs: NonNegativeInt = 4   # OPT-BE: exact minimum for G1
     multiplier_collision_broad_phase: PositiveInt = 4  # OPT: halve broadphase buffer (+0.58%)
     integrator: gs.integrator = gs.integrator.approximate_implicitfast
     IK_max_targets: PositiveInt = 6
